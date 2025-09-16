19316cedfedbc5be59c744a9b7342f929632ffff
Milen Valev <milennv@yahoo.com>
2025-09-16 23:53:05 +0300

    Create 07_projects_creation.py

---
+++ 07_projects_creation.py
@@ -0,0 +1,7 @@
+name = input()
+number_of_projects = int(input())
+hours_per_project = 3
+
+needed_hours = number_of_projects * hours_per_project
+
+print(f"The architect {name} will need {needed_hours} hours to complete {number_of_projects} project/s.")
