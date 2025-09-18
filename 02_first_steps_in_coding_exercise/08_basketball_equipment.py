0be64ee95321cb181a72cdc7e029a4bd402b9f62
Milen Valev <milennv@yahoo.com>
2025-09-18 23:00:33 +0300

    Create 08_basketball_equipment.py

---
+++ 02_first_steps_in_coding_exercise/08_basketball_equipment.py
@@ -0,0 +1,10 @@
+basketball_annual_fee = int(input())
+
+basketball_shoes = basketball_annual_fee * 0.60
+basketball_fit = basketball_shoes * 0.80
+basketball_ball = basketball_fit / 4
+basketball_accessories = basketball_ball / 5
+
+total = basketball_annual_fee + basketball_shoes + basketball_fit + basketball_ball + basketball_accessories
+
+print(total)
